bec1abf8b923483222d86a943add8c57023bc816
Paweł <skibaapawell@gmail.com>
2020-04-17 14:11:23 +0200

    Dostosowanie klasy Construction, zmiana podejscia (rysowania lini -> rysowanie wielokatow).

--- VirtualCamera_Painters_Algorithm/src/camera/Construction.java
+++ VirtualCamera_Painters_Algorithm/src/camera/Construction.java
@@ -16,19 +16,16 @@ import java.util.List;
  */
 public class Construction {
 
-    private List<Line3D> lines3D;
-    private List<Line2D> lines2D;
+    private ArrayList<Figure> figures;
 
     public Construction() {
-        lines3D = new ArrayList<>();
-        lines2D = new ArrayList<>();
+        figures = new ArrayList<>();
     }
 
     public Construction(String adress) {
         try {
             ReadFile readFile = new ReadFile(adress);
-            lines3D = readFile.load();
-            lines2D = new ArrayList<>();
+            figures = readFile.load();
         } catch (FileNotFoundException e) {
             System.out.println("The file was not found!");
         } catch (IOException e) {
@@ -36,23 +33,7 @@ public class Construction {
         }
     }
 
-    public List<Line3D> getLines3D() {
-        return lines3D;
-    }
-
-    public List<Line2D> getLines2D() {
-        return lines2D;
-    }
-
-    public void addLine3D(Line3D line) {
-        lines3D.add(line);
-    }
-
-    public void addLine2D(Line2D line) {
-        lines2D.add(line);
-    }
-
-    public void clearLines2D() {
-        lines2D = new ArrayList<>();
+    public List<Figure> getFigures() {
+        return figures;
     }
 }

2723ef4fccba431023314c6c5f6eb6953c49dffb
Paweł <skibaapawell@gmail.com>
2020-05-11 10:39:30 +0200

    Usuniecie konstruktora. Dodanie metody sortFigures()

--- VirtualCamera_Painters_Algorithm/src/camera/Construction.java
+++ VirtualCamera_Painters_Algorithm/src/camera/Construction.java
@@ -8,6 +8,7 @@ package camera;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,10 +19,6 @@ public class Construction {
 
     private ArrayList<Figure> figures;
 
-    public Construction() {
-        figures = new ArrayList<>();
-    }
-
     public Construction(String adress) {
         try {
             ReadFile readFile = new ReadFile(adress);
@@ -31,9 +28,17 @@ public class Construction {
         } catch (IOException e) {
             System.out.println("Wrong format of file!");
         }
+        sortFigures();
     }
 
     public List<Figure> getFigures() {
         return figures;
+    }
+
+    public void sortFigures() {
+        for (Figure figure : figures) {
+            figure.searchCentroid();
+        }
+        Collections.sort(figures);
     }
 }
